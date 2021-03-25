0d9168389848bb0bcc9ad0815570af72a794858c
kboeije <kjellboeije@gmail.com>
2021-03-25 23:09:12 +0100

    Cleaned up client file

--- client.py
+++ client.py
@@ -3,8 +3,6 @@
 # Basic
 import os
 import asyncio
-import signal
-import pickle
 
 # Discord
 import discord
@@ -16,35 +14,33 @@ from quiz import *
 
 TOKEN_DISCORD_BOT = os.getenv("TOKEN_DISCORD_BOT")
 
-
-def create_bot():
-    bot = commands.Bot(command_prefix='!', help_command=None)
-
-    @bot.event
-    async def on_ready():
-        print('Logged in as {}'.format(bot.user))
-        await bot.change_presence(activity=discord.Game("Mex"))
-
-    @bot.event
-    async def on_command_error(ctx, error):
-        if isinstance(error, CommandNotFound):
-            return
-        raise error
-
-    bot.add_cog(Mex(bot))
-    bot.add_cog(Quiz(bot))
-    return bot
+bot = commands.Bot(command_prefix='!', help_command=None)
 
 
-def cleanup(bot):
-    bot.get_cog('Mex').cleanup()
+@bot.event
+async def on_ready():
+    print('Logged in as {}'.format(bot.user))
+    await bot.change_presence(activity=discord.Game("Mex"))
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, CommandNotFound):
+        return
+    raise error
+
+
+bot.add_cog(Mex(bot))
+bot.add_cog(Quiz(bot))
+
+
+def cleanup(mexbot):
+    # Invoke saving ChannelController states
+    mexbot.get_cog('Mex').cleanup()
     print('*In Luigi voice*: "Bye bye"')
 
 
-# create_bot().run(TOKEN_DISCORD_BOT)
-bot = create_bot()
 loop = asyncio.get_event_loop()
-# loop.add_signal_handler(signal.SIGINT, cleanup)
 try:
     loop.run_until_complete(bot.start(TOKEN_DISCORD_BOT))
 except KeyboardInterrupt:
